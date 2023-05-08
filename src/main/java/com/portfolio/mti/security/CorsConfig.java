3807356b85d2c1a1df963a821e2926c0ada570d4
Matias <matiasc.02.2003@gmail.com>
2023-05-08 03:08:26 -0300

    feat : backend finished 🛠

--- src/main/java/com/portfolio/mti/security/CorsConfig.java
+++ src/main/java/com/portfolio/mti/security/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("https://portfolio-front-caa7e.web.app/")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .exposedHeaders("Authorization");
--- src/main/java/com/portfolio/mti/security/WebSecurityConfig.java
+++ src/main/java/com/portfolio/mti/security/WebSecurityConfig.java
@@ -61,5 +61,4 @@ public class WebSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-
 }
